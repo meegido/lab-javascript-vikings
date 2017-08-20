c67040224d0aacff7c2e84042a3505ea8a7094a9
Mercedes Egido <meegido@gmail.com>
2017-08-20 22:53:59 +0200

    Vikings iteration

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,43 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
+
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= 50;
+}
 
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.battleCry = function() {
+    return "Odin Owns You All!";
+  }
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.attack = function(){
+  return this.strength;
+}
+
+Viking.prototype.receiveDamage = function(damage, health) {
+  this.health -= 50;
+
+  if(this.health < this.strength) {
+    return this.name + " has received " + damage + " points of damage";
+  } else {
+    return this.name + " has died in act of combat";
+  }
+
+}
 
 // Saxon
 function Saxon () {}
