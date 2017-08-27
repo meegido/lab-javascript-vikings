feab9441f6370e0948ecb63f149a59ce8ab2c957
Mercedes Egido <meegido@gmail.com>
2017-08-27 19:33:25 +0200

    saxon finished

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -28,19 +28,39 @@ Viking.prototype.attack = function(){
   return this.strength;
 }
 
-Viking.prototype.receiveDamage = function(damage, health) {
-  this.health -= 50;
+Viking.prototype.receiveDamage = function(damage) {
+  console.log(this.health);
+  this.health -= damage;
 
-  if(this.health < this.strength) {
+  if(this.health > 0) {
     return this.name + " has received " + damage + " points of damage";
   } else {
-    return this.name + " has died in act of combat";
+    return this.name + " has died in act of combat"
   }
 
 }
 
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+  Soldier.call(this, health, strength)
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+Saxon.prototype.attack = function() {
+  return this.strength;
+}
+
+Saxon.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+
+    if(this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else {
+      return "A Saxon has died in combat"
+    }
+}
 
 // War
 function War () {}
